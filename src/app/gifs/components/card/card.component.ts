1bb7b8827ea1295097fe8f20b52254c2e05aea63
Cesar Ceron <ceron.cmcr@gmail.com>
2024-05-25 13:33:08 -0600

    Curso Angular - Seccion 8

---
+++ src/app/gifs/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif, The480_WStill } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './card.component.html'
+})
+export class CardComponent implements OnInit {
+  @Input()
+  public gif!: Gif;
+
+  constructor() { }
+  ngOnInit(): void {
+    if (!this.gif) {
+      throw new Error('Gif property is required');
+    }
+  }
+}
